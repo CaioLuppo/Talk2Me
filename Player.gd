038f21c05ba4aec4d4bf13b539fcb871adf1695a
CaioLuppo <luppocode@outlook.com>
2022-06-06 16:48:31 -0300

    Organização do código e melhora nos sprites

--- Player.gd
+++ Player.gd
@@ -1,14 +1,59 @@
 extends KinematicBody2D
 
+var speed = Vector2()
+
+var current_state := 0 # estado atual
+enum {WALK, IDLE}
+
+var enter_state = true
+
 func _process(delta):
+	match current_state:
+		WALK:
+			_walk()
+			pass
+		IDLE:
+			_idle()
+			pass
+
+# Funções de estado
+func _walk():
+	move()
+	_move_and_slide()
+	_set_state(_check_walk_state())
+	
+func _idle():
+	$anim.play('idle')
+	speed = Vector2(0,0)
+	_set_state(_check_idle_state())
+
+# Helpers
+func _move_and_slide():
+	speed = move_and_slide(speed, Vector2.UP)
+
+func move():
 	if Input.is_action_pressed("ui_down"):
-		walking()
-	else:
-		idle()
+		speed.y = 105
+		$anim.play('down')
+	if Input.is_action_pressed("ui_up"):
+		speed.y = -105
+		$anim.play('up')
 
-func walking():
-	position.y += 2
-	$sprite.play('down')
+func _set_state(new_state):
+	if new_state != current_state:
+		enter_state = true
+	current_state = new_state
 
-func idle():
-	$sprite.play('idle')
+
+# Funções de checagem (O que pode tirar o player de tal estado?)
+func _check_idle_state():
+	var new_state = current_state
+	if Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_down"):
+		new_state = WALK
+	return new_state
+
+func _check_walk_state():
+	var new_state = current_state
+	if !Input.is_action_pressed("ui_up") && !Input.is_action_pressed("ui_down"):
+		new_state = IDLE
+	return new_state
